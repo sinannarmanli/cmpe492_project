cba075c2eb3ed52dac8c4758ab32292ef06d7110
Murat Guvenc <muratguvenc1905@gmail.com>
2013-03-22 16:42:32 +0200

    Scaler added
    simple scaler added. Improvements are needed.

---
+++ bitirme/JS/app.js
@@ -0,0 +1,143 @@
+﻿/// <reference path="../../../Scripts/angular.1.09.js" />
+
+function DiffusionController($scope) {
+
+    var mean, stdDev, numberOfMolecules, initialX, initialY, distanceBetweenTwoCells, environmentMoleculeSize, boltzmanCoeff, 
+		temperature, viscosity, moleculeSize, cellRadius;
+
+    mean = 0;
+    numberOfMolecules = 100;
+    initialX = 300;
+    initialY = 200;
+	distanceBetweenTwoCells = 10;	//mikrometre olsun
+	cellRadius = 15;
+    environmentMoleculeSize = 1.32; //nanometer
+    boltzmanCoeff = 0.000086173324;
+    temperature = 310; //kelvin
+    viscosity = 0.001; // ( kg / (s * m) )
+    moleculeSize = 1;   //nanometer
+
+    $scope.view = {
+        molecules: [],
+        iterationTime: 0.5,   //sec
+        numberOfMolecules: 100,
+        maxTime: 40000,
+        currentTime: 0,
+		distanceBetweenTwoCells: 10,
+		cellRadius: 15,
+		temperature: 310,
+		viscosity: 0.001,
+		moleculeSize: 1,
+        show:true
+    };
+
+    function generateGaussianRandom(stdDev) {
+
+        var u1, u2, randStdNormal;
+        u1 = Math.random();
+        u2 = Math.random();
+        randStdNormal = Math.sqrt(-2.0 * Math.log(u1)) * Math.sin(2.0 * Math.PI * u2); //random normal(0,1);
+        return (mean + stdDev * randStdNormal);
+    }
+
+    function caculatePropagationStandardDeviation(time) {
+
+        if (environmentMoleculeSize > moleculeSize + 0.1) {
+            return Math.sqrt(((boltzmanCoeff * temperature)
+                    / (3 * Math.PI * viscosity * moleculeSize)) * time);
+        }
+        return Math.sqrt(((boltzmanCoeff * temperature)
+                    / (2 * Math.PI * viscosity * moleculeSize)) * time);
+    }
+
+    function drawAllMolecules() {
+        var moleculesLocal;
+
+        moleculesLocal = $scope.view.molecules;     //for performance increase
+
+        for (var i = 0; i < moleculesLocal.length; i++) {
+            drawCircle(moleculesLocal[i].x, moleculesLocal[i].y, 1);
+        }
+    };
+
+    function iterateMolecules() {
+        var moleculesLocal, stdDev, gaussianRandomX, gaussianRandomY;
+
+        stdDev = caculatePropagationStandardDeviation($scope.view.iterationTime);
+
+        moleculesLocal = $scope.view.molecules;
+
+        for (var i = 0; i < moleculesLocal.length; i++) {
+            moleculesLocal[i].x += generateGaussianRandom(stdDev);
+            moleculesLocal[i].y += generateGaussianRandom(stdDev);
+        }
+
+        $scope.view.molecules = moleculesLocal;
+    };
+
+    $scope.init = function () {
+
+        var moleculesLocal, numberOfMoleculesLocal, distanceBetweenTwoCellsLocal, radiusT, radiusR, temp;
+		
+		distanceBetweenTwoCellsLocal = $scope.view.distanceBetweenTwoCells;
+		moleculesLocal = [];
+        numberOfMoleculesLocal = $scope.view.numberOfMolecules;
+		cellRadiusLocal =  calculateRadius(distanceBetweenTwoCellsLocal);
+		
+		temp = 300 - Number(distanceBetweenTwoCellsLocal)/2;
+		drawCircle(175,200,cellRadiusLocal);		//Draw receiver cell
+		drawCircle(300,200,cellRadiusLocal);			//Draw transmitter cell
+        //create molecules
+        for (var i = 0; i < numberOfMoleculesLocal; i++) {
+            moleculesLocal.push({ x: initialX-Number(cellRadiusLocal), y: initialY });
+        }
+
+        $scope.view.molecules = moleculesLocal;
+        $scope.view.show = false;
+
+        initRaphael();
+
+        drawAllMolecules();
+    };
+	
+	function calculateRadius(distance){
+		if(distance < 5)
+			return 45;
+		else if( distance > 15 )
+			return 15;
+		else{
+			return (225/Number(distance));
+		}
+	};
+    
+    $scope.drawRandomMolecule = function () {
+
+        var x = Math.floor(Math.random() * 600);    // max 600 because of canvas size, see raphael.js under Js folder
+        var y = Math.floor(Math.random() * 400);
+        drawCircle(x, y, 1);
+    };
+
+    $scope.iterateSimulation = function () {
+
+        var localTime = 0;
+
+        var iterate = function () {
+            clearPaper();
+            iterateMolecules();
+            drawAllMolecules();
+            localTime += 5;
+
+            if (localTime % 100) {
+                $scope.$apply($scope.view.currentTime = localTime / 100);
+            }
+
+            if (!(localTime > 1000)) {
+                setTimeout(function () { iterate(); }, 50);
+            }
+        }
+
+        iterate();
+    };
+
+
+};
---
+++ bitirme/JS/raphael.js
@@ -0,0 +1,67 @@
+﻿/// <reference path="../../../Scripts/AjaxLogin.js" />
+/// <reference path="../../../Scripts/underscore.min.js" />
+/// <reference path="../../../Scripts/angular-mocks-0.9.19.js" />
+/// <reference path="../../../Scripts/angular.1.09.js" />
+/// <reference path="../../../Scripts/angular.resource.js" />
+/// <reference path="../../../Scripts/jquery-1.6.2-vsdoc.js" />
+/// <reference path="../../../Scripts/jquery-1.6.2.js" />
+/// <reference path="../../../Scripts/jquery-1.6.2.min.js" />
+/// <reference path="../../../Scripts/jquery-ui-1.8.11.js" />
+/// <reference path="../../../Scripts/jquery-ui-1.8.11.min.js" />
+/// <reference path="../../../Scripts/jquery.unobtrusive-ajax.js" />
+/// <reference path="../../../Scripts/jquery.unobtrusive-ajax.min.js" />
+/// <reference path="../../../Scripts/raphael-min.js" />
+/// <reference path="../../../Scripts/jquery.validate-vsdoc.js" />
+/// <reference path="../../../Scripts/jquery.validate.js" />
+/// <reference path="../../../Scripts/jquery.validate.min.js" />
+/// <reference path="../../../Scripts/jquery.validate.unobtrusive.js" />
+/// <reference path="../../../Scripts/jquery.validate.unobtrusive.min.js" />
+/// <reference path="../../../Scripts/knockout-2.0.0.debug.js" />
+/// <reference path="../../../Scripts/knockout-2.0.0.js" />
+/// <reference path="../../../Scripts/less-1.3.0.min.js" />
+/// <reference path="../../../Scripts/modernizr-2.0.6-development-only.js" />
+/// <reference path="../../../Scripts/raphael-min.js" />
+
+/*function cLog(args) {
+    for (var i = 0; i < arguments.length; i++) {
+        console.log(arguments[i]);
+    }
+}*/
+
+
+var paper, drawings;
+
+
+function initRaphael () {
+    //paper = Raphael($(".raphael")[0], 406, 664);
+    paper = Raphael(0, 0, 600, 400);    
+}
+
+function drawCircle(x,y,r) {
+    // Creates circle at x = 50, y = 40, with radius 10
+    var circle = paper.circle(x, y, r);
+    // Sets the fill attribute of the circle to red (#f00)
+    circle.attr("fill", "#f00");
+
+    // Sets the stroke attribute of the circle to white
+    circle.attr("stroke", "#000");
+}
+
+function drawScaler() {
+	var scaler = paper.path("M 400 40 l 0 15 l 0 -5 l 50 0 l 0 5 l 0 -15 l 0 10 l 50 0 l 0 -10 l 0 10 l 50 0 l 0 -10 z");
+	var inScaler = paper.path("M 450 40 l 5 10 l 5 0 l -5 -10 l 5 10 l 5 0 l -5 -10 l 5 10 l 5 0 l -5 -10 l 5 10 l 5 0 l -5 -10 l 5 10 l 5 0 l -5 -10 l 5 10 l 5 0 l -5 -10 l 5 10 l 5 0 l -5 -10 l 5 10 l 5 0 l -5 -10 l 5 10 l 5 0 l -5 -10 ");
+	var text1 = paper.text(425, 58, '1 cm').attr({"font-size": 11,"font-weight": "bold"});
+	var text2 = paper.text(560, 45, 'µm').attr({"font-size": 11,"font-weight": "bold"});
+	var text3;
+	for (var i = 0; i<4; i+=1){
+		paper.text(400+i*50, 30, i*100).attr({"font-size": 11,"font-weight": "bold"});
+	}
+}
+
+function clearPaper() {
+    paper.clear();
+}
+
+window.onload = function () {
+    initRaphael();
+};
